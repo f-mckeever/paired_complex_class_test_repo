b7f1dd4581acdc6e9f62d1e852ad30e073cf9006
fionnmckeever <fmckeever04@qub.ac.uk>
2025-10-15 15:44:11 +0100

    Completed tests for add_new_one, add_new_several

--- tests/test_most_often.py
+++ tests/test_most_often.py
@@ -16,16 +16,49 @@ add_new
 get_most_often
 -> store a list
 -> goes through the hole list -> loop test
--> needs a place to store the count -> is the output the same as the expected output 
+-> needs a place to store the count -> is the output the 
+same as the expected output 
 outputs 
     1) the highest 
     2) "no clear winner"
 """
 from lib.most_often import *
 
+#__init__
 def test_MostOften_initantiates():
     list = []
     mo = MostOften(list)
     assert isinstance(mo, MostOften)
 
-def test_MostOften_
+def test_MostOften_starting_list_is_a_list():
+    test_list = []
+    mo = MostOften(test_list)
+    assert isinstance(mo.starting_list, list)
+
+def test_MostOften_start_state():
+    test_list = [1, 2, 3]
+    mo = MostOften(test_list)
+    assert len(mo.starting_list) == 3
+
+
+#add_new
+def test_MostOften_add_new_one_more():
+    test_list = [1, 2, 3]
+    new_item = [4]
+    mo = MostOften(test_list)
+    mo.add_new(new_item)
+    assert len(test_list) == 4
+
+def test_MostOften_add_new_several_more():
+    test_list = [1, 2, 3]
+    new_item = 4
+    new_item2 = 5
+
+
+    mo = MostOften(test_list)
+
+    mo.add_new(new_item)
+
+    mo.add_new(new_item2)
+
+    assert len(test_list) == 5
