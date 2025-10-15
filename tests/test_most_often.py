d19962a3342293b79108865cdf505b2fd09cceb5
David Woods <woodsdw@gmail.com>
2025-10-15 14:51:34 +0100

    initantiates test completes

--- tests/test_most_often.py
+++ tests/test_most_often.py
@@ -7,17 +7,25 @@ __init__
 -> is starting_list a list? []
 -> what type is each item?
 
+
 add_new
+-> check what the new item is // is it a valid item? 
 -> len(list) before == 1 less than len(list) after
 -> validate new_item == item_list[-1]
 
 get_most_often
 -> store a list
-
-
-
-
-
-
-
+-> goes through the hole list -> loop test
+-> needs a place to store the count -> is the output the same as the expected output 
+outputs 
+    1) the highest 
+    2) "no clear winner"
 """
+from lib.most_often import *
+
+def test_MostOften_initantiates():
+    list = []
+    mo = MostOften(list)
+    assert isinstance(mo, MostOften)
+
+def test_MostOften_
